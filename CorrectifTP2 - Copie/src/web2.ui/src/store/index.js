2b3cd99ebea4b1343a5f2424eb50329b4463b12a
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-11-02 17:17:14 -0400

    commit 53

--- CorrectifTP2 - Copie/src/web2.ui/src/store/index.js
+++ CorrectifTP2 - Copie/src/web2.ui/src/store/index.js
@@ -1,11 +1,29 @@
 import { createStore } from 'vuex'
 
 export default createStore({
+  strict: true,
   state: {
+    evenements: []
   },
   getters: {
+    getProgress(state) {
+      return (state.evenements.filter(t => t.done).length / state.evenements.length) * 100
+    }
   },
   mutations: {
+    setEvenements(state, evenements) {
+      state.evenements = evenements
+    },
+    createEvenement(state, evenement) {
+      state.evenements.push(evenement)
+    },
+    deleteEvenement(state, index) {
+      state.evenements.splice(index, 1)
+    },
+    updateStatus(state, index) {
+      const evenement = state.evenements[index]
+      evenement.done = !evenement.done
+    }
   },
   actions: {
   },
