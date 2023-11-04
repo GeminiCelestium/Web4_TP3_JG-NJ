07a4ef35d3479d5630228cf9636e0cb771b99abb
Nathan Joseph <2145612@cegeplimoilou.ca>
2023-11-03 21:36:13 -0400

    Debut page accueil

--- CorrectifTP2/src/web2.ui/src/components/CompAccueil.vue
+++ CorrectifTP2/src/web2.ui/src/components/CompAccueil.vue
@@ -2,33 +2,27 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Voici notre TP3. Nous vous prévenons tout de suite
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Présentation du TP3</h3>
+    <p>
+      Texte Inutile
+    </p>
+    <h3>Participants</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><h4>Jason Gravel</h4></li>
+      <li><h4>Nathan Joseph</h4></li>
     </ul>
-    <h3>Essential Links</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+      <li>
+        
+        <img alt="Image Jason" src="../assets/jason.gif">
+      </li>
+      <li>
+        
+        <img alt="Portrait Nathan" src="../assets/nathan.avif">
+      </li>
+    </ul>        
   </div>
 </template>
 
@@ -45,6 +39,10 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
+}
+h4 {
+  margin-right: 290px;
+  margin-left: 190px;
 }
 ul {
   list-style-type: none;
--- CorrectifTP2/src/web2.ui/src/views/VueAccueil.vue
+++ CorrectifTP2/src/web2.ui/src/views/VueAccueil.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <Accueil msg="Welcome to Your Vue.js App"/>
+    <Accueil msg="Bienvenue dans notre projet Vue.js"/>
   </div>
 </template>
 
